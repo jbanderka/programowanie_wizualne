f76ba45cbefcc353913fd4e559fd07cef778a5d7
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-10-11 16:47:42 +0200

    Add files via upload

---
+++ lab1/lab1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer dTimer = new DispatcherTimer();
+            dTimer.Tick += tick;
+            dTimer.Interval = TimeSpan.FromSeconds(1);
+            dTimer.Start();
+        }
+
+        void tick(object sender, EventArgs e)
+        {
+            Clock.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void Stoper_Click(object sender, RoutedEventArgs e)
+        {
+            var timerWindow = new Window();
+            timerWindow.Show();
+            
+        }
+    }
+}
