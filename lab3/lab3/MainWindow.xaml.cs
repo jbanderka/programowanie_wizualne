37b67ab09fb7b0ac9128a5f9569b06c1b8ed81a2
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-10-25 16:44:07 +0200

    Add files via upload

---
+++ lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int id = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            listV.ItemsSource = new List<Row>();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAdd windowAdd = new WindowAdd(this);
+            windowAdd.Show();
+        }
+
+    }
+
+    public class Row
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ lab3/lab3/WindowAdd.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Interaction logic for WindowAdd.xaml
+    /// </summary>
+    public partial class WindowAdd : Window
+    {
+        MainWindow mainWindow;
+        public WindowAdd(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void addRow_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.id += 1;
+            List<Row> items = (List<Row>)mainWindow.listV.ItemsSource;
+            items.Add(new Row() { Name = addName.Text, Id = mainWindow.id, Count = int.Parse(addCount.Text) });
+            mainWindow.listV.ItemsSource = items;
+            this.Close();
+        }
+    }
+}
