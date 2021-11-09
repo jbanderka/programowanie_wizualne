da2583e7ce77f151cb1d6ebac448e8a88299a01b
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-11-09 14:30:31 +0100

    final version of project from lab4

--- lab4/lab4/MainWindow.xaml.cs
+++ lab4/lab4/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using Microsoft.Win32;
 using System.IO;
 using System.Drawing;
 using System.Windows.Interop;
+using System.Drawing.Imaging;
 
 namespace lab4
 {
@@ -26,6 +27,9 @@ namespace lab4
     {
         Bitmap bitmap;
         BitmapImage bitmapimage;
+        int flipped = 1;
+        bool onlyGreen = false;
+        BitmapImage savedImage;
 
         public MainWindow()
         {
@@ -39,20 +43,25 @@ namespace lab4
                 BitmapEncoder enc = new BmpBitmapEncoder();
                 enc.Frames.Add(BitmapFrame.Create(bitmapImage));
                 enc.Save(outStream);
-                Bitmap bitmap = new Bitmap(outStream);
+                Bitmap btmp = new Bitmap(outStream);
 
-                return new Bitmap(bitmap);
+                return new Bitmap(btmp);
             }
         }
 
-        private BitmapImage Bitmap2BitmapImage(Bitmap bitmap)
+        private BitmapImage Bitmap2BitmapImage()
         {
-            BitmapSource i = Imaging.CreateBitmapSourceFromHBitmap(
-                           bitmap.GetHbitmap(),
-                           IntPtr.Zero,
-                           Int32Rect.Empty,
-                           BitmapSizeOptions.FromEmptyOptions());
-            return (BitmapImage)i;
+            BitmapImage bitmapImage = new BitmapImage();
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+            }
+            return bitmapImage;
         }
 
         private void Choose_Click(object sender, RoutedEventArgs e)
@@ -60,26 +69,107 @@ namespace lab4
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true)
             {
+                RotateTransform rotate = img.LayoutTransform as RotateTransform;
+                rotate = new RotateTransform(0, 0, 0);
+                img.LayoutTransform = rotate;
                 Uri fileUri = new Uri(openFileDialog.FileName);
                 bitmapimage = new BitmapImage(fileUri);
-                bitmap = new Bitmap(bitmapimage.StreamSource);
+                savedImage = bitmapimage;
+                bitmap = BitmapImage2Bitmap(bitmapimage);
                 img.Source = bitmapimage;
             }
         }
 
         private void mirror_Click(object sender, RoutedEventArgs e)
         {
-            /*Bitmap imgBitmap = BitmapImage2Bitmap(img);
-            var flippedImage = new TransformedBitmap(img.Source, new ScaleTransform(-1, 1));*/
+            if(bitmap != null)
+            {
+                bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                bitmapimage = Bitmap2BitmapImage();
+                img.Source = bitmapimage;
+                flipped *= -1;
+            }
         }
 
         private void turn_Click(object sender, RoutedEventArgs e)
         {
-            /*TransformedBitmap myRotatedBitmapSource = new TransformedBitmap();
-            myRotatedBitmapSource.BeginInit();
-            myRotatedBitmapSource.Source = img.Source;
-            myRotatedBitmapSource.Transform = new RotateTransform(90);
-            myRotatedBitmapSource.EndInit();*/
+            if(bitmap != null)
+            {
+                RotateTransform rotate = img.LayoutTransform as RotateTransform;
+                if (rotate == null)
+                {
+                    if(flipped == 1)
+                    {
+                        rotate = new RotateTransform(-90, 0, 0);
+                    }
+                    if(flipped == -1)
+                    {
+                        rotate = new RotateTransform(90, 0, 0);
+                    }
+                    img.LayoutTransform = rotate;
+                }
+                else
+                {
+                    if (flipped == 1)
+                    {
+                        rotate.Angle -= 90;
+                    }
+                    if (flipped == -1)
+                    {
+                        rotate.Angle += 90;
+                    }
+                }
+            }
+        }
+
+        private void green_Click(object sender, RoutedEventArgs e)
+        {
+            if(bitmap != null)
+            {
+                if(onlyGreen)
+                {
+                    bitmap = BitmapImage2Bitmap(savedImage);
+                    onlyGreen = false;
+                }
+                else
+                {
+                    System.Drawing.Color c;
+                    for (int i = 0; i < bitmap.Width; i++)
+                    {
+                        for (int j = 0; j < bitmap.Height; j++)
+                        {
+                            c = bitmap.GetPixel(i, j);
+                            if (c.R < 50 && c.G < 150 && c.B < 50)
+                            {
+                                c = System.Drawing.Color.FromArgb(255, 255, 255);
+                                bitmap.SetPixel(i, j, c);
+                            }
+                        }
+                    }
+                    onlyGreen = true;
+                }
+                bitmapimage = Bitmap2BitmapImage();
+                img.Source = bitmapimage;
+            }
+        }
+
+        private void neg_Click(object sender, RoutedEventArgs e)
+        {
+            if(bitmap != null)
+            {
+                System.Drawing.Color c;
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        c = bitmap.GetPixel(i, j);
+                        c = System.Drawing.Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                        bitmap.SetPixel(i, j, c);
+                    }
+                }
+                bitmapimage = Bitmap2BitmapImage();
+                img.Source = bitmapimage;
+            }
         }
     }
 }
