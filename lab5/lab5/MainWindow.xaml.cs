068b4ec4fc5f4939dccddc85187f5aab939b1738
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-11-15 13:09:15 +0100

    initial commit from lab5

---
+++ lab5/lab5/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog sequenceFile = new OpenFileDialog();
+            if (sequenceFile.ShowDialog() == true)
+            {
+                string seqText = File.ReadAllText(sequenceFile.FileName);
+                string noWhiteSpaces = String.Concat(seqText.Where(c => !Char.IsWhiteSpace(c)));
+                sequenceBox.Text = noWhiteSpaces;
+            }
+        }
+
+        private void find_Click(object sender, RoutedEventArgs e)
+        {
+            Algorithm algorithm = new Algorithm(sequenceBox.Text, 3);
+        }
+    }
+
+    public class Algorithm
+    {
+        public Algorithm(string sequence, int k)
+        {
+            findPatterns(sequence, k);
+        }
+
+        private void findPatterns(string sequence, int k)
+        {
+            string foundPatterns = "";
+
+        }
+    }
+}
