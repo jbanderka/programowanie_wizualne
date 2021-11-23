0d3fd8d31396b8536b17c24d2a941194f3104a0a
jbanderka <55949860+jbanderka@users.noreply.github.com>
2021-11-23 17:12:32 +0100

    final version of project from lab6

--- lab6/lab3/MainWindow.xaml.cs
+++ lab6/lab3/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,8 @@ using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 using System.Xml.Serialization;
+using System.Text.RegularExpressions;
+using System.Reflection;
 
 namespace lab6
 {
@@ -24,16 +28,46 @@ namespace lab6
     public partial class MainWindow : Window
     {
         public int id = 0;
+        public List<Row> allItems;
         public MainWindow()
         {
             InitializeComponent();
             listV.ItemsSource = new List<Row>();
+            NameValueCollection sAll;
+            sAll = ConfigurationManager.AppSettings;
+            string lastPath = sAll.Get("LastPath");
+            if(lastPath != "")
+            {
+                listV.ItemsSource = new List<Row>();
+                string fileData;
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(string));
+                StreamReader reader = new StreamReader(lastPath);
+                fileData = (string)xmlSerializer.Deserialize(reader);
+                char[] delims = new[] { '\r', '\n' };
+                string[] entries = fileData.Split(delims, StringSplitOptions.RemoveEmptyEntries);
+                id = 0;
+                foreach (string entry in entries)
+                {
+                    string[] fields = entry.Split(',');
+                    id += 1;
+                    List<Row> items = (List<Row>)listV.ItemsSource;
+                    items.Add(new Row() { Name = fields[0], Id = int.Parse(fields[1]), Count = int.Parse(fields[2]) });
+                    listV.ItemsSource = items;
+                    listV.Items.Refresh();
+                }
+                if (allItems != null)
+                    allItems.Clear();
+                allItems = (List<Row>)listV.ItemsSource;
+                id = listV.Items.Count;
+            }
+            
         }
 
         private void add_Click(object sender, RoutedEventArgs e)
         {
             WindowAdd windowAdd = new WindowAdd(this);
             windowAdd.Show();
+            allItems = (List<Row>)listV.ItemsSource;
         }
 
         private void save_Click(object sender, RoutedEventArgs e)
@@ -48,12 +82,11 @@ namespace lab6
             XmlSerializer xmlSerializer = new XmlSerializer(data.GetType());
             SaveFileDialog saveDialog = new SaveFileDialog();
             saveDialog.Filter = "XML files(.xml)|*.xml|all Files(*.*)|*.*";
-            if (saveDialog.ShowDialog() == true)
+            if(saveDialog.ShowDialog() == true)
             {
                 TextWriter writer = new StreamWriter(saveDialog.FileName);
                 xmlSerializer.Serialize(writer, data);
                 writer.Close();
-                //File.WriteAllText(saveDialog.FileName, csvData.ToString());
             }
         }
 
@@ -79,8 +112,66 @@ namespace lab6
                     listV.ItemsSource = items;
                     listV.Items.Refresh();
                 }
+                if(allItems != null)
+                    allItems.Clear();
+                allItems = (List<Row>)listV.ItemsSource;
                 id = listV.Items.Count;
+
+                Configuration configuration = ConfigurationManager.OpenExeConfiguration(Assembly.GetExecutingAssembly().Location);
+                configuration.AppSettings.Settings["LastPath"].Value = openDialog.FileName;
+                configuration.Save();
+                ConfigurationManager.RefreshSection("appSettings");
             }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Czy chcesz zapisać zawartość?",
+                "Zapis", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if(result == MessageBoxResult.Yes)
+            {
+                save.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+            }
+        }
+
+        private void search_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = toSearch.Text;
+            if (Regex.IsMatch(searchText, @"^[0-9]+$"))
+            {
+                // searchText is numeric
+                int searchNumber = int.Parse(searchText);
+                List<Row> filteredItems = new List<Row>();
+                foreach (Row listRow in listV.ItemsSource)
+                {
+                    if (listRow.Count == searchNumber)
+                        filteredItems.Add(listRow);
+                }
+                listV.ItemsSource = filteredItems;
+            }
+            else if (Regex.IsMatch(searchText, @"^[a-zA-Z]+$"))
+            {
+                // searchText contains only letters
+                List<Row> filteredItems = new List<Row>();
+                foreach (Row listRow in listV.ItemsSource)
+                {
+                    if (listRow.Name == searchText)
+                        filteredItems.Add(listRow);
+                }
+                listV.ItemsSource = filteredItems;
+            }
+            else if(searchText != "")
+            {
+                // searchText contains numbers, letters (and maybe special characters)
+                MessageBox.Show("Tekst do wyszukiwania musi zawierać tylko litery lub tylko cyfry!", "Błąd wyszukiwania",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void clearSearch_Click(object sender, RoutedEventArgs e)
+        {
+            toSearch.Text = "";
+            listV.ItemsSource = allItems;
         }
     }
 
