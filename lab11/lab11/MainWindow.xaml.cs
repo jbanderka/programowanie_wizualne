2864ebb5417047342e1d64fa1793231d93b1572e
jbanderka <55949860+jbanderka@users.noreply.github.com>
2022-01-03 16:59:24 +0100

    initial commit of project from lab11

---
+++ lab11/lab11/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void hideText_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void saveText_Click(object sender, RoutedEventArgs e)
+        {
+            string dataToSave = outputText.Text;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "txt files(.txt)|*.txt|all Files(*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+                File.WriteAllText(saveFileDialog.FileName, dataToSave);
+        }
+    }
+}
