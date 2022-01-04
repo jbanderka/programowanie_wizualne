6310a7166db1df07c75fd16febc66f1d36914d61
jbanderka <55949860+jbanderka@users.noreply.github.com>
2022-01-04 16:15:15 +0100

    final version of project from lab11

--- lab11/lab11/MainWindow.xaml.cs
+++ lab11/lab11/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Security.Cryptography;
 
 namespace lab11
 {
@@ -29,7 +30,48 @@ namespace lab11
 
         private void hideText_Click(object sender, RoutedEventArgs e)
         {
+            string textToHide = inputText.Text;
+            string keyText = key.Text;
+            if(textToHide == "" || keyText == "")
+            {
+                outputText.Text = "";
+                MessageBoxResult result = MessageBox.Show("Brak wprowadzonego tekstu do szyfrowania lub klucza szyfrującego. Wprowadź wszystkie potrzebne dane.",
+                        "Brak danych", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                if (keyText.Length == 16 || keyText.Length == 24 || keyText.Length == 32)
+                {
+                    using (Aes myAes = Aes.Create())
+                    {
+                        byte[] encrypted;
+                        byte[] aesKey = Encoding.UTF8.GetBytes(keyText);
+                        byte[] aesIV = Enumerable.Repeat((byte)0x0, 16).ToArray();
 
+                        myAes.Key = aesKey;
+                        myAes.IV = aesIV;
+                        ICryptoTransform encryptor = myAes.CreateEncryptor(myAes.Key, myAes.IV);
+                        using (MemoryStream msEncrypt = new MemoryStream())
+                        {
+                            using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                                {
+                                    swEncrypt.Write(textToHide);
+                                }
+                                encrypted = msEncrypt.ToArray();
+                            }
+                        }
+                        outputText.Text = Convert.ToBase64String(encrypted);
+                    }
+                }
+                else
+                {
+                    outputText.Text = "";
+                    MessageBoxResult result = MessageBox.Show("Wprowadzony klucz ma złą długość. Wprowadź klucz o poprawnej długości.",
+                        "Zła długość klucza", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }           
         }
 
         private void saveText_Click(object sender, RoutedEventArgs e)
